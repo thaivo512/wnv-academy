8e7bff675baefb868e0c405a18713b3a32333df9
thang1998 <nkoxken65@gmail.com>
2021-01-10 23:17:30 +0700

    dropdown categoires

--- frontend/academy/src/containers/student/home-page.js
+++ frontend/academy/src/containers/student/home-page.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, Form, FormControl, Button, Dropdown } from 'react-bootstrap';
+import '../../assets/student.scss';
+import { FaShoppingCart } from 'react-icons/fa';
 
 class StudentHomePage extends Component {
     constructor(props) {
@@ -7,7 +10,47 @@ class StudentHomePage extends Component {
 
     render() {
         return (
-            <div>adsad</div>
+            <Navbar bg="light" variant="light">
+                <Navbar.Brand href="#home">Udemy</Navbar.Brand>
+                <Nav className="nav-mr-right">
+                    <Dropdown className="dropdown">
+                        <Dropdown.Toggle className="custom-radius dropdown-toggle" variant="outline-info" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Categories
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu className="dropdown-menu">
+                            <Dropdown.Item>Design</Dropdown.Item>
+                            <Dropdown.Item>Development</Dropdown.Item>
+                            <Dropdown.Item>English</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+                    {/* <div class="dropdown">
+                        <button class="btn btn-info dropdown-toggle custom-radius" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Categories
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div> */}
+                </Nav>
+                <Form className="form-wrap-input" inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2 custom-input" />
+                    <Button className="custom-radius" variant="info">Search</Button>
+                </Form>
+                <Nav className="nav-wrap-button justify-content-end" >
+                    <Nav.Item className="nav-icon-mr-right">
+                        <FaShoppingCart className="icon-shopping-cart" />
+                    </Nav.Item>
+                    <Nav.Item className="nav-mr-right">
+                        <Button variant="outline-info">Login</Button>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Button variant="info">Sign up</Button>
+                    </Nav.Item>
+                </Nav>
+            </Navbar>
         )
     }
 }
